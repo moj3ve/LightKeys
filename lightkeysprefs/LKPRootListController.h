e2d9f96f1008f667d4d22d25b6734ea67ca9da8e
Mathieu <matranc03@gmail.com>
2019-11-10 19:26:10 -0500

    respring button + other prefs

--- lightkeysprefs/LKPRootListController.h
+++ lightkeysprefs/LKPRootListController.h
@@ -1,5 +1,9 @@
 #import <Preferences/PSListController.h>
+#import "NSTask.h"
 
 @interface LKPRootListController : PSListController
+
+@property (nonatomic, retain) UIBarButtonItem *respringButton;
+- (void)respring:(id)sender;
 
 @end
--- lightkeysprefs/LKPRootListController.m
+++ lightkeysprefs/LKPRootListController.m
@@ -1,6 +1,8 @@
 #include "LKPRootListController.h"
 
 @implementation LKPRootListController
+@synthesize respringButton;
+
 
 - (NSArray *)specifiers {
 	if (!_specifiers) {
@@ -8,6 +10,13 @@
 	}
 
 	return _specifiers;
+}
+
+- (void)respring:(id)sender {
+    NSTask *task = [[[NSTask alloc] init] autorelease];
+    [task setLaunchPath:@"/usr/bin/killall"];
+    [task setArguments:[NSArray arrayWithObjects:@"backboardd", nil]];
+    [task launch];
 }
 
 @end
